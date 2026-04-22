58eb8367055b4ece3d5a59d6054f9b06bcf2da35
Le Phuoc Loc <lephuocloc00z12@gmail.com>
2026-04-22 13:37:22 +0700

    bai2

---
+++ bai2.sql
@@ -0,0 +1,14 @@
+CREATE TABLE CUSTOMERS (
+	CustomerID INT PRIMARY KEY,
+    FullName VARCHAR(100),
+    Email VARCHAR(100),
+    Age int
+);
+
+-- ràng buộc độc nhất
+ALTER TABLE CUSTOMERS
+ADD constraint UQ_Email UNIQUE (Email);
+
+-- ràng buộc tuổi
+ALTER TABLE CUSTOMERS
+ADD constraint CK_Age check (age > 0);
